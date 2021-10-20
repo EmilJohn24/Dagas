7691eb978db53f94e85f04ae3787e2d1f0ac1d1a
Emil John Lopez <emiljohn24@gmail.com>
2021-10-20 11:21:29 +0800

    Added sample models (Donations) and added relief to main URLConf

--- DagasServer/relief/models.py
+++ DagasServer/relief/models.py
@@ -1,4 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
-#TODO: Create models
+# See management/commands/create_groups.py for list of groups
+# TODO: Create models
+
+class Donation(models.Model):
+    donor = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'groups__name': 'donors'})
+    datetime_added = models.DateTimeField('Date added')
+
+
+# TODO: Add some kind of type to Supply
+class Supply(models.Model):
+    name = models.CharField(max_length=250)
+    quantity = models.IntegerField
+    pax = models.IntegerField
+    donation = models.ForeignKey(Donation)
