105f5f8353176dd0d1d7c9a3a35877c4958a1fc2
Emil John Lopez <emiljohn24@gmail.com>
2021-11-24 15:41:05 +0800

    Fixed branching

---
+++ DagasAndroid/app/src/main/java/com/cnil/dagas/http/OkHttpSingleton.java
@@ -0,0 +1,25 @@
+package com.cnil.dagas.http;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class OkHttpSingleton extends OkHttpClient {
+    private final String BASE_URL = "http://192.168.100.2:8000";
+    public final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    private static class LazyHolder{
+        private static final OkHttpSingleton client = new OkHttpSingleton();
+    }
+
+    public static OkHttpSingleton getInstance(){
+        return LazyHolder.client;
+    }
+
+    public Request.Builder builderFromBaseUrl(String subURL){
+        return new Request.Builder().url(BASE_URL + subURL);
+    }
+
+
+}
