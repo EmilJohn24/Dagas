236b2e4e62af6a27c6bac3f2a899bae0820ee630
Josef Lyle H. Solis <jhefy.solis@gmail.com>
2021-11-30 17:06:27 +0800

    updated QR scanner fragment

--- DagasAndroid/app/src/main/java/com/cnil/dagas/ui/home/resident/qrscanner.java
+++ DagasAndroid/app/src/main/java/com/cnil/dagas/ui/home/resident/qrscanner.java
@@ -1,39 +1,104 @@
 package com.cnil.dagas.ui.home.resident;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.cnil.dagas.R;
 import com.cnil.dagas.databinding.EvacuationcentervisualmapBinding;
 import com.cnil.dagas.databinding.QrScannerBinding;
+import com.google.zxing.Result;
+
+import java.util.ArrayList;
+
 
 public class qrscanner extends Fragment {
 
+//    private final static int CAMERA_REQUEST_CODE = 101;
+
     private QrScannerBinding binding;
+    private CodeScanner mCodeScanner;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final Activity activity = getActivity();
         binding = QrScannerBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        if(ContextCompat.checkSelfPermission(activity.getApplicationContext(),Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+        }
+        int result = ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.READ_PHONE_STATE);
+            if (result == 0){
+        // function which uses the permission
+            CodeScannerView scannerView = root.findViewById(R.id.scanner_view);
+            mCodeScanner = new CodeScanner(activity, scannerView);
+            mCodeScanner.setDecodeCallback(new DecodeCallback() {
+                @Override
+                public void onDecoded(@NonNull final Result result) {
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(activity, result.getText(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
+            scannerView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mCodeScanner.startPreview();
+                }
+            });
+        }
 
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
         return root;
     }
+
+
+
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        mCodeScanner.startPreview();
+//    }
+//
+//    @Override
+//    public void onPause() {
+//        mCodeScanner.releaseResources();
+//        super.onPause();
+//    }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+//    private void setupPermissions(){
+//        int permission = ContextCompat.checkSelfPermission(getActivity(),
+//                android.Manifest.permission.CAMERA);
+//
+//        if(permission != PackageManager.PERMISSION_GRANTED){
+//            makerequest();
+//        }
+//    }
+
+//    private void makerequest(){
+//        ActivityCompat.requestPermissions(this,
+//                new ArrayList(android.Manifest.permission.CAMERA),
+//                CAMERA_REQUEST_CODE);
+//    }
 }
