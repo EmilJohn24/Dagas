81901ecce8083c87d6addcf37a6ae089861db8d8
Emil John Lopez <emiljohn24@gmail.com>
2022-06-29 18:26:29 +0800

    REACT: Created sample EvacCenter getter in React

--- dagas-react/src/App.js
+++ dagas-react/src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component{
+  //retrieves the attributes of the tag and initializes the object
+  constructor(props){
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      evacuationCenters: [],
+    }
+  };
+  refreshEvacuationCenters = () => {
+    axios
+      .get("/relief/api/evacuation-center/")
+      .then((result) => this.setState({evacuationCenters: result.data}))
+      .catch((error) => console.log(error));
+  }
+  renderEvacuationCenter = () => {
+    return this.state.evacuationCenters.map((item) => (
+      <li>{item.name}</li>
+    ))
+  }
+  //runs when component works
+  componentDidMount(){
+    this.refreshEvacuationCenters();
+  }
+
+  //runs when class tag is called elsewhere
+  render(){
+    return (
+      <main className="container">
+        <h1>Evacuation Centers</h1>
+        <ul className="list-group">{this.renderEvacuationCenter()}</ul>
+      </main>
+    )
+  }
+
+
 }
-
 export default App;
--- dagas-react/src/index.js
+++ dagas-react/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css'; //Bootstrap
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// Good tutorial: https://www.digitalocean.com/community/tutorials/build-a-to-do-application-using-django-and-react#step-3-setting-up-the-frontend
+// Official website tutorial: https://reactjs.org/docs/getting-started.html
+// Another tutorial: https://blog.logrocket.com/creating-an-app-with-react-and-django/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
