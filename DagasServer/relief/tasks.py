3428b9b93e0bb2e1e379f66660dbbf88a143ac92
Emil John Lopez <emiljohn24@gmail.com>
2022-07-07 15:59:11 +0800

    FIX: Celery successfully integrated to project and algorithm function added

--- DagasServer/relief/tasks.py
+++ DagasServer/relief/tasks.py
@@ -3,6 +3,10 @@ from celery import shared_task
 
 # app = Celery('tasks', broker='pyamqp://guest@localhost//')
 # https://docs.celeryq.dev/en/stable/getting-started/next-steps.html#next-steps
+# https://docs.celeryq.dev/en/stable/getting-started/first-steps-with-celery.html#first-steps
+# https://docs.celeryq.dev/en/latest/django/first-steps-with-django.html#using-celery-with-django
+from relief.models import DonorProfile, BarangayRequest
+
 
 @shared_task
 def add(x, y):
@@ -17,3 +21,11 @@ def mul(x, y):
 @shared_task
 def xsum(numbers):
     return sum(numbers)
+
+
+@shared_task
+def algorithm():
+    donors = DonorProfile.objects.all()
+    barangay_requests = BarangayRequest.objects.all()
+    print(donors[0])
+    return
