e6ad139189b384a4b8e399ffc26c1c3bd4b32cfe
Josef Lyle H. Solis <jhefy.solis@gmail.com>
2022-07-18 20:08:03 +0800

    Added draw route on Transaction Receipt

--- DagasAndroid/app/src/main/java/com/cnil/dagas/TransactionReceiptFragment.java
+++ DagasAndroid/app/src/main/java/com/cnil/dagas/TransactionReceiptFragment.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 
@@ -36,13 +37,26 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.MediaType;
 import okhttp3.Request;
@@ -208,11 +222,22 @@ public class TransactionReceiptFragment extends Fragment  implements OnMapReadyC
             map.addMarker(new MarkerOptions()
                             .position(evacLatLng)
                             .title(name));
-            map.animateCamera(CameraUpdateFactory.newLatLngZoom(evacLatLng, 1));
+            //map.animateCamera(CameraUpdateFactory.newLatLngZoom(evacLatLng, 1));
+
+            //Direction Draw: https://stackoverflow.com/questions/47492459/how-do-i-draw-a-route-along-an-existing-road-between-two-points?fbclid=IwAR1adU2y6vUzwdKUPmcNjc44v43mD_omM7QmfJiyuUWUyaQfZGN3hFzzVFg
+            //Define list to get all latlng for the route
+            List<LatLng> path = new ArrayList();
+
+            //Execute Directions API request
+            GeoApiContext context = new GeoApiContext.Builder()
+                    .apiKey("AIzaSyBqxOriSUSwlm8HEZ0W6gkQj3fazIbegDM")
+                    .build();
 
             //Loader: https://stackoverflow.com/questions/10207612/android-execute-code-in-regular-intervals
             final int donorUserID = thread.getTransactionJSON().getJSONObject("donor_info").getInt("user");
             final Handler handler = new Handler();
+            final boolean[] firstAnimateCamera = {false};
+            final Polyline[] previousPath = {null, null};
             Runnable donorLocationThread = new Runnable() {
                 @Override
                 public void run() {
@@ -224,6 +249,74 @@ public class TransactionReceiptFragment extends Fragment  implements OnMapReadyC
                         donorLocationMarker.setPosition(new LatLng(retrieveGeolocationThread.getDonorLat(),
                                                             retrieveGeolocationThread.getDonorLong()));
 
+                        DirectionsApiRequest req = DirectionsApi.getDirections(context,retrieveGeolocationThread.getDonorLat() + "," + retrieveGeolocationThread.getDonorLong(),
+                                latitude + "," + longtitude);
+
+                        DirectionsResult res = null;
+                        try {
+                            res = req.await();
+                        } catch (ApiException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        if(previousPath[0] != null || previousPath[1] != null) {
+                            previousPath[0].remove();
+                            previousPath[1].remove();
+                            path.clear();
+                        }
+                        //Loop through legs and steps to get encoded polylines of each step
+                        if (res.routes != null && res.routes.length > 0) {
+                            DirectionsRoute route = res.routes[0];
+
+                            if (route.legs !=null) {
+                                for(int i=0; i<route.legs.length; i++) {
+                                    DirectionsLeg leg = route.legs[i];
+                                    if (leg.steps != null) {
+                                        for (int j=0; j<leg.steps.length;j++){
+                                            DirectionsStep step = leg.steps[j];
+                                            if (step.steps != null && step.steps.length >0) {
+                                                for (int k=0; k<step.steps.length;k++){
+                                                    DirectionsStep step1 = step.steps[k];
+                                                    EncodedPolyline points1 = step1.polyline;
+                                                    if (points1 != null) {
+                                                        //Decode polyline and add points to list of route coordinates
+                                                        List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                                        for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                            path.add(new LatLng(coord1.lat, coord1.lng));
+                                                        }
+                                                    }
+                                                }
+                                            } else {
+                                                EncodedPolyline points = step.polyline;
+                                                if (points != null) {
+                                                    //Decode polyline and add points to list of route coordinates
+                                                    List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                                    for (com.google.maps.model.LatLng coord : coords) {
+                                                        path.add(new LatLng(coord.lat, coord.lng));
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        //Draw the polyline
+                        if (path.size() > 0) {
+                            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+                            previousPath[0] = map.addPolyline(opts);
+                            previousPath[1] = map.addPolyline(opts);
+                        }
+
+                        if(!firstAnimateCamera[0]) {
+                            map.getUiSettings().setZoomControlsEnabled(true);
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(retrieveGeolocationThread.getDonorLat(),
+                                    retrieveGeolocationThread.getDonorLong()), 15));
+                            firstAnimateCamera[0] = true;
+                        }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     } finally{
