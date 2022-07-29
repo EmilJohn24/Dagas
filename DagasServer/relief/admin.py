03afea566705ac862f42aba0e2c6beaea3e1f356
Emil John Lopez <emiljohn24@gmail.com>
2022-07-29 22:06:11 +0800

    ADD: Added a number of features to the admin page

--- DagasServer/relief/admin.py
+++ DagasServer/relief/admin.py
@@ -5,21 +5,11 @@ from django.contrib.auth.admin import UserAdmin
 from django_google_maps import widgets as map_widgets
 from django_google_maps import fields as map_fields
 from relief.models import User, Transaction, EvacuationCenter, BarangayProfile, DonorProfile, Donation, Supply, \
-    BarangayRequest, ItemRequest, RouteNode, RouteSuggestion, Fulfillment, Disaster
+    BarangayRequest, ItemRequest, RouteNode, RouteSuggestion, Fulfillment, Disaster, ResidentProfile
+
 # Guide: https://docs.djangoproject.com/en/4.0/ref/contrib/admin/
 # 3.2 Guide: https://docs.djangoproject.com/en/3.2/ref/contrib/admin/
-admin.site.register(User, UserAdmin)
-admin.site.register(Transaction)
-admin.site.register(BarangayProfile)
-admin.site.register(DonorProfile)
-admin.site.register(Donation)
-admin.site.register(Supply)
-admin.site.register(BarangayRequest)
-admin.site.register(ItemRequest)
-admin.site.register(RouteNode)
-admin.site.register(RouteSuggestion)
-admin.site.register(Fulfillment)
-admin.site.register(Disaster)
+# admin.site.register(User, UserAdmin)
 admin.site.site_header = 'Dagas DSWD Administation System'
 
 
@@ -29,4 +19,30 @@ class EvacuationCenterAdmin(admin.ModelAdmin):
     }
 
 
+class ProfileAdmin(UserAdmin):
+    list_filter = ('role', 'is_staff',)
+
+
+class DonorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'current_disaster',)
+
+
+class SupplyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'quantity', 'pax', 'calculate_available_pax', 'donation',)
+    list_filter = ('type',)
+
+
+admin.site.register(Transaction)
+admin.site.register(BarangayProfile)
+admin.site.register(DonorProfile, DonorAdmin)
+admin.site.register(ResidentProfile)
+admin.site.register(Donation)
+admin.site.register(Supply, SupplyAdmin)
+admin.site.register(BarangayRequest)
+admin.site.register(ItemRequest)
+admin.site.register(RouteNode)
+admin.site.register(RouteSuggestion)
+admin.site.register(Fulfillment)
+admin.site.register(Disaster)
+admin.site.register(User, ProfileAdmin)
 admin.site.register(EvacuationCenter, EvacuationCenterAdmin)
