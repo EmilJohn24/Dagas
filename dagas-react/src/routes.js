21c85259475446269aadca63e7f352befcc41d32
Josef Lyle H. Solis <jhefy.solis@gmail.com>
2022-08-19 12:45:51 +0800

    added api connection for barangay request

--- dagas-react/src/routes.js
+++ dagas-react/src/routes.js
@@ -69,7 +69,7 @@ import ResetCover from "layouts/authentication/reset-password/cover";
 import Login from './components/Login';
 import TransactionList from "layouts/transactions/transaction-list";
 import SupplyList from "layouts/supply/supply-list";
-import BarangayRequest from "components/BarangayRequest"
+import BarangayRequest from "layouts/barangay/request"
 import RequestList from "layouts/barangay/request-list"
 
 // Material Dashboard 2 PRO React components
