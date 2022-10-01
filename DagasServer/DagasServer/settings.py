235949e8a55cc1942005c280c042d850def150b9
Emil John Lopez <emiljohn24@gmail.com>
2022-10-01 14:51:40 +0800

    ADD: Broker URL to settings

--- DagasServer/DagasServer/settings.py
+++ DagasServer/DagasServer/settings.py
@@ -280,6 +280,7 @@ SIMPLE_JWT = {
 # Celery
 # CELERY_BROKER_URL = 'redis://localhost:6379'
 # CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = os.environ.get('CLOUDAMQP_URL')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
