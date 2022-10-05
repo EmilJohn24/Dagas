2b80add73849516580a99a39ba829a6124444f54
Emil John Lopez <emiljohn24@gmail.com>
2022-10-05 20:36:54 +0800

    MOD: Changed test user to FakeDonor0

--- DagasServer/tests.py
+++ DagasServer/tests.py
@@ -7,10 +7,11 @@ import django
 django.setup()
 from relief.models import DonorProfile
 from relief.tasks import solo_algo_tests, generate_data
-
-generate_data(donor_count=1, evacuation_center_count=20,
-              min_demand=10, max_demand=30,
-              min_supply=75, max_supply=100)
+print("Generating dataset...")
+# generate_data(donor_count=1, evacuation_center_count=70,
+#               min_demand=10, max_demand=30,
+#               min_supply=150, max_supply=200)
 print("Done generating dataset...")
-donor_id = DonorProfile.objects.all()[0].pk
+# donor_id = DonorProfile.objects.all()[0].pk
+donor_id = DonorProfile.objects.get(user__username="FakeDonor0").id
 solo_algo_tests("tabu", donor_id, None)
