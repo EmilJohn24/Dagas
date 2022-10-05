0060c906e9246d13030a04c65ad54d565dc185dd
Emil John Lopez <emiljohn24@gmail.com>
2022-10-05 08:23:58 +0800

    MOD: Changed from debug mode to production mode

--- DagasServer/DagasServer/settings.py
+++ DagasServer/DagasServer/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_-rs#!5yr7=4%1z2e89schctuhz4wows3xb-6j3f4xfe$074jd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.100.2', '192.168.1.4', '192.168.31.25', '127.0.0.1', '192.168.2.108', '192.168.1.87', '192.168.1.63',]
 
