9543d9f907ac5e2c9d32181e6a1ab178379fa612
Emil John Lopez <emiljohn24@gmail.com>
2022-11-10 19:22:23 +0800

    MOD: Extended access token lifetime to 120 minutes

--- DagasServer/DagasServer/settings.py
+++ DagasServer/DagasServer/settings.py
@@ -264,7 +264,7 @@ CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOW_ALL_ORIGINS = True
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
